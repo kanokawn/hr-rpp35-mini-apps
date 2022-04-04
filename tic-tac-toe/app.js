424bacefcf07854d780f722544e2276545daaf70
Kevin On <idealistkevin@gmail.com>
2022-04-03 22:16:46 -0700

    Refactored to create table in app.js, added formatted table, and working towards wins/ties

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,51 +1,87 @@
 let eventCounter = 0;
+let ticTacToeObj = {
+  0: ['', '', ''],
+  1: ['', '', ''],
+  2: ['', '', '']
+};
 
-console.log('it has been set up');
+let app = document.getElementById('app');
 
-// function placeX (event) {
-//   if (eventCounter === 9) {
-//     alert('The game has finished');
-//   }
-//   if (eventCounter % 2 === 0) {
-//     eventCounter++;
-//     return 'X'
-//   } else {
-//     eventCounter++;
-//     return 'O'
-//   }
-// }
+var createTable = function () {
+  let table = document.createElement('table');
+  table.setAttribute('id', 'tic-tac-toe');
+  table.style.border = '1px solid';
+  table.style.width = '500px';
+  table.style.height = '500px';
+  table.style.textAlign = 'center';
+  for (var i = 0; i < 3; i++) {
+    let tr = document.createElement('tr');
+    tr.uniqueId = i;
+    for (var j = 0; j < 3; j++) {
+      let td = document.createElement('td');
+      td.style.height = '100px';
+      td.style.width = '100px';
+      td.style.backgroundColor = 'pink';
+      td.style.fontSize = '50px';
+      td.uniqueId = j;
+      tr.appendChild(td);
+    }
+    table.appendChild(tr);
+  }
+  app.appendChild(table);
+}
 
-// var ticTacToeTable = document.getElementById('tic-tac-toe');
-// console.log(ticTacToeTable);
-// var trs = ticTacToeTable.getElementsByTagName('tr');
-// console.log(trs);
-// var tds = null;
-
-// for (var i = 0; i < trs.length; i++) {
-//   tds = trs[i].getElementsByTagName('td');
-//   for (var j = 0; j < tds.length; j++) {
-//     tds[j].onclick = placeX;
-//     console.log(tds);
-//   }
-// }
+createTable();
 
 var cells = document.querySelectorAll('td');
+
+var checkDiag = function (mark) {
+  var win = true;
+  let currentRow = 0;
+  let currentCol = 0;
+  for (var i = 0; i < 3; i++) {
+    win = win && (ticTacToeObj[currentRow][currentCol] === mark);
+    currentRow++;
+    currentCol++;
+  };
+  if (win) {
+    return win;
+  } else {
+    currentRow = 0;
+    currentCol = 2;
+    for (var i = 0; i < 3; i++) {
+      win = win && (ticTacToeObj[currentRow][currentCol] === mark);
+      currentRow++;
+      currentCol++;
+    };
+    return win;
+  }
+};
+var checkVertical = function (mark, x, y) {};
+var checkHorizontal = function (mark, x, y) {};
+
 for (var i = 0; i < cells.length; i++) {
   cells[i].onclick = function(event) {
-    if (eventCounter === 9) {
-      alert('The game has finished');
-    }
-    if (eventCounter % 2 === 0) {
-      eventCounter++;
-      this.innerHTML = 'X';
+    if (this.innerHTML === '') {
+      if (eventCounter % 2 === 0) {
+        eventCounter++;
+        this.innerHTML = 'X';
+        ticTacToeObj[this.parentNode.uniqueId][this.uniqueId] = 'X';
+        console.log(ticTacToeObj);
+        console.log(checkDiag('X'));
+      } else {
+        eventCounter++;
+        this.innerHTML = 'O';
+        ticTacToeObj[this.parentNode.uniqueId][this.uniqueId] = 'O';
+        console.log(ticTacToeObj);
+        console.log(checkDiag('X'));
+      }
     } else {
-      eventCounter++;
-      this.innerHTML = 'O';
+      if (eventCounter === 9) {
+        alert('The game has finished.');
+      } else {
+        alert('A move has already been made here.')
+      }
     }
   }
-  console.log('event listener added!');
 }
-console.log(cells);
-
-var cells2 = document.querySelectorAll('td');
-console.log(cells2);
