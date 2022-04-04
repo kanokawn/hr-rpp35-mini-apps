ab8ac1c6ebf3b3bb16bcb4a1ddb0e23aa0319c7e
Kevin On <idealistkevin@gmail.com>
2022-04-04 00:00:55 -0700

    Complete Bare Minimum Requirements

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,11 +1,18 @@
+/////////////////////////////////////////////
+//////////// Initial Variables //////////////
+/////////////////////////////////////////////
+let app = document.getElementById('app');
 let eventCounter = 0;
+let winner;
 let ticTacToeObj = {
   0: ['', '', ''],
   1: ['', '', ''],
   2: ['', '', '']
 };
 
-let app = document.getElementById('app');
+/////////////////////////////////////////////
+////////// Creating DOM Elements /////////////
+/////////////////////////////////////////////
 
 var createTable = function () {
   let table = document.createElement('table');
@@ -33,54 +40,109 @@ var createTable = function () {
 
 createTable();
 
-var cells = document.querySelectorAll('td');
-
-var checkDiag = function (mark) {
-  var win = true;
-  let currentRow = 0;
-  let currentCol = 0;
-  for (var i = 0; i < 3; i++) {
-    win = win && (ticTacToeObj[currentRow][currentCol] === mark);
-    currentRow++;
-    currentCol++;
-  };
-  if (win) {
-    return win;
-  } else {
-    currentRow = 0;
-    currentCol = 2;
-    for (var i = 0; i < 3; i++) {
-      win = win && (ticTacToeObj[currentRow][currentCol] === mark);
-      currentRow++;
-      currentCol++;
+var createResetButton = function () {
+  let reset = document.createElement('button');
+  let br = document.createElement('br');
+  reset.setAttribute('id', 'reset');
+  reset.innerHTML = 'Reset the Game';
+  reset.style.height = '100px';
+  reset.style.width = '200px';
+  reset.style.fontSize = '20px'
+  reset.onclick = function () {
+    ticTacToeObj = {
+      0: ['', '', ''],
+      1: ['', '', ''],
+      2: ['', '', '']
     };
-    return win;
+    cells.forEach(cell => {
+      cell.innerHTML = '';
+    });
+    eventCounter = 0;
+    winner = undefined;
   }
+  app.appendChild(br);
+  app.appendChild(reset);
+}
+
+createResetButton();
+
+
+/////////////////////////////////////////////
+//////// Tic - Tac - Toe Functions //////////
+/////////////////////////////////////////////
+
+//////// Checking Win Status //////////
+
+var checkDiag = function (mark, column) {
+  let currentCol = column;
+  for (var i = 0; i < 3; i++) {
+    if (ticTacToeObj[i][currentCol] === mark) {
+    } else {
+      return false;
+    }
+    if (column === 0) {
+      currentCol++;
+    } else {
+      currentCol--;
+    }
+  }
+  return true;
 };
-var checkVertical = function (mark, x, y) {};
-var checkHorizontal = function (mark, x, y) {};
+
+var checkVertical = function (mark, column) {
+  for (var i = 0; i < 3; i++) {
+    if (ticTacToeObj[i][column] !== mark) {
+      return false
+    }
+  }
+  return true;
+};
+var checkHorizontal = function (mark, row) {
+  for (var i = 0; i < 3; i++) {
+    if (ticTacToeObj[row][i] !== mark) {
+      return false
+    }
+  }
+  return true;
+};
+
+var checkWin = function (mark, row, column) {
+  return checkDiag(mark, 0) || checkDiag(mark, 2) || checkVertical(mark, column) || checkHorizontal(mark, row);
+}
+
+//////// Playing The Game //////////
+
+let cells = document.querySelectorAll('td');
 
 for (var i = 0; i < cells.length; i++) {
   cells[i].onclick = function(event) {
-    if (this.innerHTML === '') {
+    let row = Number(this.parentNode.uniqueId);
+    let column = Number(this.uniqueId);
+    if (this.innerHTML === '' && !winner) {
       if (eventCounter % 2 === 0) {
         eventCounter++;
         this.innerHTML = 'X';
-        ticTacToeObj[this.parentNode.uniqueId][this.uniqueId] = 'X';
-        console.log(ticTacToeObj);
-        console.log(checkDiag('X'));
+        ticTacToeObj[row][column] = 'X';
+        if (checkWin('X', row, column)) {
+          alert('X has won!')
+          winner = 'X';
+        };
       } else {
         eventCounter++;
         this.innerHTML = 'O';
         ticTacToeObj[this.parentNode.uniqueId][this.uniqueId] = 'O';
-        console.log(ticTacToeObj);
-        console.log(checkDiag('X'));
+        if (checkWin('O', row, column)) {
+          alert('O has won!')
+          winner = 'O';
+        };
       }
     } else {
       if (eventCounter === 9) {
-        alert('The game has finished.');
+        alert('There is a tie!');
+      } else if (winner) {
+        alert(`Player ${winner} has won!`);
       } else {
-        alert('A move has already been made here.')
+        alert('A mark has already been placed here.');
       }
     }
   }
